90230a26e570e0e6e0554590c215a3b49134c643
WinstonT <tandiw22@gmail.com>
2024-02-26 00:08:16 +0700

    fix navbar styling

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,11 +1,14 @@
 import Link from "next/link";
-import React from "react";
 
 export default function Navbar() {
   return (
     <div className="flex justify-end space-x-4">
-      <Link href="/">Work</Link>
-      <Link href="/about">About</Link>
+      <Link href="/" legacyBehavior>
+        <a className={"no-underline hover:underline"}>Work</a>
+      </Link>
+      <Link href="/about" legacyBehavior>
+        <a className={"no-underline hover:underline"}>About</a>
+      </Link>
     </div>
   );
 }
