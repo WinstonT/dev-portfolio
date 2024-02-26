e7d94bb4f9626ff6f1d49e2fe22b6feef24adc39
WinstonT <tandiw22@gmail.com>
2024-02-26 17:53:39 +0700

    add projects page

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,6 +6,9 @@ export default function Navbar() {
       <Link href="/" legacyBehavior>
         <a className={"no-underline hover:underline"}>Work</a>
       </Link>
+      <Link href="/projects" legacyBehavior>
+        <a className={"no-underline hover:underline"}>Projects</a>
+      </Link>
       <Link href="/about" legacyBehavior>
         <a className={"no-underline hover:underline"}>About</a>
       </Link>
