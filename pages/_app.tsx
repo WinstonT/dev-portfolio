10e29308bc05608164b49906611476bcd908b492
WinstonT <tandiw22@gmail.com>
2024-02-26 17:20:19 +0700

    fix favicon logo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,6 +7,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
         <link
           rel="alternate"
           type="application/rss+xml"
