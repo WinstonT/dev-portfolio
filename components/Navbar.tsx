110a9edfc4d9d2a7d41928a774ba3a97edbff1ac
WinstonT <tandiw22@gmail.com>
2024-10-29 20:55:38 +0700

    update navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,13 +3,13 @@ import Link from "next/link";
 export default function Navbar() {
   return (
     <div className="flex justify-end space-x-4">
-      <Link href="/" legacyBehavior>
+      <Link href="/work" legacyBehavior>
         <a className={"no-underline hover:underline"}>Work</a>
       </Link>
       <Link href="/projects" legacyBehavior>
         <a className={"no-underline hover:underline"}>Projects</a>
       </Link>
-      <Link href="/about" legacyBehavior>
+      <Link href="/" legacyBehavior>
         <a className={"no-underline hover:underline"}>About</a>
       </Link>
     </div>
